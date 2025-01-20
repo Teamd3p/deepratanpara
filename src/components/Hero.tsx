65493bc7fc30e4390ffd72a5ee96f6ce87ca8c4a
Deep Ratanpara <149156828+Teamd3p@users.noreply.github.com>
2025-01-20 16:07:45 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
     // Create a link element
     const link = document.createElement('a');
     link.href = '/lovable-uploads/Deep_Resume.pdf'; // Make sure to upload your resume with this exact name
-    link.download = 'Deep_Resume.pdf';
+    link.download = 'Resume_deep.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
